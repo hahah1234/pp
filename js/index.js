d3cdc4bdbc3f97174b60dbd783dccf6ae9317e66
hahah1234 <45236120+hahah1234@users.noreply.github.com>
2018-11-23 18:15:08 +0900

    dd

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $(window).on("click,touchstart",function(){
+    $(window).on("click,touchstart,tap",function(){
         console.log($(".backstretch img").attr("src"));
         var imgPath = $(".backstretch img").attr("src");
 
